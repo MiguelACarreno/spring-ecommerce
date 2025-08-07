2d891a5176dd779ca2b14571df2febe438c21226
MiguelACarreno <migueluchi08@gmail.com>
2025-08-07 18:17:50 -0500

    Implementación para guardar la imagen del producto

--- src/main/java/com/ecommerce/controller/ProductoControlador.java
+++ src/main/java/com/ecommerce/controller/ProductoControlador.java
@@ -3,6 +3,7 @@ package com.ecommerce.controller;
 import com.ecommerce.model.Producto;
 import com.ecommerce.model.Usuario;
 import com.ecommerce.service.ProductoService;
+import com.ecommerce.service.UploadFileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,9 +11,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import org.slf4j.*;
 
+import java.io.IOException;
 import java.util.Optional;
 
 @Controller
@@ -24,6 +28,9 @@ public class ProductoControlador {
 
     @Autowired
     private ProductoService productoService;
+
+    @Autowired
+    private UploadFileService upload;
 
     @GetMapping("")
     public String show(Model model) {
@@ -38,10 +45,25 @@ public class ProductoControlador {
 
 
     @PostMapping("/save")
-    public String save(Producto producto) {
+    public String save(Producto producto,@RequestParam("img") MultipartFile file) throws IOException {
         logger.info("Guardando producto: {}", producto);
         Usuario u = new Usuario(1,"","","","","","","");
         producto.setUsuario(u);
+
+        //imagen
+        if (producto.getId() == null) { // cuando es nuevo un producto
+            String nombreImagen = upload.saveImage(file);
+            producto.setImagen(nombreImagen);
+        }else { // cuando se edita un producto
+            if (file.isEmpty()) { // editando sin cambiar la imagen
+                Producto p = new Producto();
+                p = productoService.getProducto(producto.getId()).get();
+                producto.setImagen(p.getImagen());
+            }else { // editando y cambiando la imagen
+                String nombreImagen = upload.saveImage(file);
+                producto.setImagen(nombreImagen);
+            }
+        }
 
         productoService.save(producto);
         return "redirect:/productos";
