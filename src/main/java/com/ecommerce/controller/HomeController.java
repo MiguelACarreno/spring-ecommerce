fc399b168b82d3368aa4b1844c1439b4a364b89b
MiguelACarreno <migueluchi08@gmail.com>
2025-08-09 19:55:32 -0500

    mostrar la vista del usuario al producto

--- src/main/java/com/ecommerce/controller/HomeController.java
+++ src/main/java/com/ecommerce/controller/HomeController.java
@@ -5,11 +5,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.slf4j.*;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+
+
+    private final Logger logger = LoggerFactory.getLogger(ProductoControlador.class);
 
     @Autowired
     private ProductoService productoService;
@@ -19,5 +25,11 @@ public class HomeController {
 
         model.addAttribute("productos", productoService.findAll());
         return "administrador/usuario/home";
+    }
+
+    @GetMapping("/productohome/{id}")
+    public String productos(@PathVariable Integer id) {
+       logger.info("Id del Producto enviado como parámetro: {}", id);
+        return "administrador/usuario/productohome";
     }
 }
