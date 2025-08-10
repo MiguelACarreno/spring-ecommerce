0d8d49972dc6013959d5c1a2ecfbc0a025450686
MiguelACarreno <migueluchi08@gmail.com>
2025-08-09 20:52:02 -0500

    redireccion al carrito de compras

--- src/main/java/com/ecommerce/controller/HomeController.java
+++ src/main/java/com/ecommerce/controller/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.slf4j.*;
@@ -41,5 +42,10 @@ public class HomeController {
         model.addAttribute("producto", producto);
 
         return "administrador/usuario/productohome";
+    }
+
+    @PostMapping("/cart")
+    public String addCart() {
+        return "administrador/usuario/carrito";
     }
 }
